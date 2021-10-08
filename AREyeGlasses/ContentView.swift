b400fbdf84b41660676e6af00860433f6831575f
Nikolay Nikitin <nikitin.nikolay.v@gmail.com>
2021-10-08 20:37:38 +0300

    Reality Kit/ SwiftUI empty template

--- AREyeGlasses/ContentView.swift
+++ AREyeGlasses/ContentView.swift
@@ -20,11 +20,7 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+     
         
         return arView
         
